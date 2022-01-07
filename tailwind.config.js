3f64874e1fab065a044ff6c25a5b247991de0fcb
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2022-01-07 12:41:59 -0500

    💄  Add styles for mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        violet: "hsl(257, 40%, 49%)",
+        softMagenta: "hsl(300, 69%, 71%)",
+      },
+      backgroundImage: {
+        mobile: "url('/images/bg-mobile.svg')",
+        desktop: "url('/images/bg-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 };
